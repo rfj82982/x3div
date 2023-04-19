e7445a1faca81e3ecd21ba99b6c21ae249f8da30
Stefano Rolfo <stefano.rolfo@stfc.ac.uk>
2023-04-19 12:22:18 +0100

    Make find_library 2decomp-fft compulsory

--- cmake/FindDECOMP2D.cmake
+++ cmake/FindDECOMP2D.cmake
@@ -1,24 +1,20 @@
-# - Find the FFTW library
+# - Find the 2decomp-fft library
 
 if( NOT D2D_ROOT AND DEFINED ENV{D2D_DIR} )
     set( D2D_ROOT $ENV{D2D_DIR} )
 endif()
+message(STATUS "D2D_ROOT ${D2D_ROOT}")
 
-if( D2D_ROOT )
-    # find libs
-    find_library(
-	DECOMP2D_FFT_LIB
-        NAMES "decomp2d" libdecomp2d
-	PATHS ${D2D_ROOT}
-        PATH_SUFFIXES "lib" "lib64"
-        NO_DEFAULT_PATH
-	REQUIRED
-        )
-    set(2decomp_INCLUDE_DIR "${D2D_ROOT}/include")
-    message(STATUS "D2D found at: ${D2D_ROOT}")
-else ()
-    message(STATUS "D2D not found builing is abort")
-
-endif()
+# find libs
+find_library(
+    DECOMP2D_FFT_LIB
+    NAMES "decomp2d" libdecomp2d
+    PATHS ${D2D_ROOT}
+    PATH_SUFFIXES "lib" "lib64"
+    NO_DEFAULT_PATH
+    REQUIRED
+    )
+set(2decomp_INCLUDE_DIR "${D2D_ROOT}/include")
+message(STATUS "D2D found at: ${D2D_ROOT}")
 
 
