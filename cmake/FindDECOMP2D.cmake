0432c50441a90d02eaa170cac8ae8ba4dfa1d510
Stefano Rolfo <stefano.rolfo@stfc.ac.uk>
2023-04-19 23:27:41 +0100

    Download and Install 2decomp-fft in case it is not avaiable on the system

--- cmake/FindDECOMP2D.cmake
+++ cmake/FindDECOMP2D.cmake
@@ -1,7 +1,26 @@
 # - Find the 2decomp-fft library
 
 if( NOT D2D_ROOT AND DEFINED ENV{D2D_DIR} )
-    set( D2D_ROOT $ENV{D2D_DIR} )
+  set( D2D_ROOT $ENV{D2D_DIR} )
+else()
+  message(STATUS "2decomp-fft PATH not available we'll try to download and install")
+  configure_file(${CMAKE_SOURCE_DIR}/cmake/decomp2d/downloadBuild2decomp.cmake.in decomp2d-build/CMakeLists.txt)
+  #message("Second CMAKE_GENERATOR ${CMAKE_GENERATOR}") 
+  execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+          RESULT_VARIABLE result
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/decomp2d-build )
+  if(result)
+      message(FATAL_ERROR "CMake step for 2decomp-fft failed: ${result}")
+  else()
+      message("CMake step for 2decomp-fft completed (${result}).")
+  endif()
+  execute_process(COMMAND ${CMAKE_COMMAND} --build .
+         RESULT_VARIABLE result
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/decomp2d-build )
+  if(result)
+      message(FATAL_ERROR "Build step for 2decomp-fft failed: ${result}")
+  endif()
+  set(D2D_ROOT ${CMAKE_CURRENT_BINARY_DIR}/decomp2d-build/downloadBuild2decomp-prefix/src/downloadBuild2decomp-build/opt)
 endif()
 message(STATUS "D2D_ROOT ${D2D_ROOT}")
 
